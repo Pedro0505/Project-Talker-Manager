309ef2c445c5363c0fe5d8be889e2c16c9199035
Pedro Henrique <pedrohrds05@hotmail.com>
2022-02-15 09:59:55 -0300

    feat: req 2

---
+++ controllers/talkerById.js
@@ -0,0 +1,19 @@
+const FILE_NAME = 'talker.json';
+const fs = require('fs/promises');
+
+async function getTalkerById(req, res) {
+  const { id } = req.params;
+
+  const data = await fs.readFile(FILE_NAME);
+  const response = JSON.parse(data);
+
+  const findTalker = response.find((e) => e.id === +id);
+
+  if (!findTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(200).json(findTalker);
+}
+
+module.exports = getTalkerById;
