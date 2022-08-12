1609dbe44b6116e7f607cd36a525832af7bce25a
pedro <pedrohrds05@hotmail.com>
2022-08-12 17:02:32 -0300

    refactor: remove trybe copy rigth

--- src/index.js
+++ src/index.js
@@ -8,13 +8,7 @@ const talkerRoutes = require('./routes/talkerRoutes');
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
 
 app.post('/login', passwordValidation, emailValidation, login);
 app.use('/talker', talkerRoutes);
